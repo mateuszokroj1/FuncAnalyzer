e6fe8b47e569bc82168f71e3c29d404c5624362d
Mateusz Okrój <mateuszokroj1@hotmail.com>
2019-01-19 22:22:17 +0100

    Building derivative

--- FuncAnalyzer/Derivative.cs
+++ FuncAnalyzer/Derivative.cs
@@ -1,28 +1,115 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq.Expressions;
 
 namespace FuncAnalyzer
 {
     public class Derivative
     {
         #region Constructor
-        public Derivative()
+        public Derivative(Expression<Func<double,double>> f)
         {
-
+            PrimaryFunction = f;
+            convertexpression();
+        }
+        public Derivative(string f)
+        {
+            PrimaryFunction = CSharpScript.EvaluateAsync<Expression<Func<double,double>>>(f).Result;
+            convertexpression();
         }
         #endregion
 
         #region Properties
-
+        public Expression<Func<double,double>> PrimaryFunction { get; protected set; }
+        protected localexpression e;
         #endregion
 
         #region Methods
+            protected void convertexpression()
+            {
 
+            }
+            public Expression<Func<double,double>> GetDerivative()
+            {
+                ParameterExpression x = Expression.Parameter(typeof(double), "x");
+                return Expression.Lambda<Func<double,double>>(e.GetDerivative(x), x);
+            }
         #endregion
 
         #region Classes
+        protected class localexpression
+        {
+            public ParameterExpression parameter;
+            public localexpression left;
+            public localexpression right;
+            public virtual Expression Original => Expression.Empty();
+            public virtual Expression GetDerivative(ParameterExpression param) { return Expression.Empty(); }
+        }
 
+        protected class constant : localexpression
+        {
+            public constant(double v) { value = v; }
+            public double value;
+            public override Expression Original => Expression.Constant(value);
+            public override Expression GetDerivative(ParameterExpression param) { return Expression.Constant(0); }
+        }
+
+        protected class parameter : localexpression
+        {
+            public new ParameterExpression Original { get; set; }
+            public override Expression GetDerivative(ParameterExpression param) { return Expression.Constant(1); }
+        }
+
+
+        protected class addition : localexpression
+        {
+            public addition(localexpression left, localexpression right) { }
+            public override Expression Original => Expression.Add(left.Original, right.Original);
+            public override Expression GetDerivative(ParameterExpression param) => Expression.Add(left.GetDerivative(param), right.GetDerivative(param));
+            
+            public override string ToString() => left.ToString() + "+" + right.ToString();
+        }
+
+        protected class substitution : localexpression
+        {
+            public substitution(localexpression left, localexpression right) { }
+            public override Expression Original => Expression.Subtract(left.Original, right.Original);
+            public override Expression GetDerivative(ParameterExpression param) => Expression.Subtract(left.GetDerivative(param), right.GetDerivative(param));
+
+            public override string ToString() => left.ToString() + "-" + right.ToString();
+        }
+
+        protected class multiplication : localexpression
+        {
+            public multiplication(localexpression left, localexpression right) { }
+            public override Expression Original => Expression.Multiply(left.Original, right.Original);
+            public override Expression GetDerivative(ParameterExpression param)
+            {
+                if (left is constant & right is parameter)
+                    return left.Original;
+                else if (left is constant & (!(right is constant) & !(right is parameter)))
+                    return Expression.Multiply(left.Original, right.GetDerivative(param));
+                else
+                    return Expression.Add(Expression.Multiply(left.GetDerivative(param), right.Original), Expression.Multiply(left.Original, right.GetDerivative(param)));
+            }
+
+            public override string ToString() => left.ToString() + "*" + right.ToString();
+        }
+
+        protected class division : localexpression
+        {
+            public division(localexpression left, localexpression right) { }
+            public override Expression Original => Expression.Divide(left.Original, right.Original);
+            public override Expression GetDerivative(ParameterExpression param) => 
+                Expression.Divide(
+                    Expression.Subtract(
+                        Expression.Multiply(left.GetDerivative(param),right.Original),
+                        Expression.Multiply(left.Original,right.GetDerivative(param))),
+                    Expression.Call(null, typeof(Math).GetMethod("Pow"), right.Original, Expression.Constant(2)));
+
+            public override string ToString() => left.ToString() + "/" + right.ToString();
+        }
         #endregion
     }
 }
