333c0f2018e1a667829a7722e9ea06948d202e32
Mateusz Okrój <mateuszokroj1@hotmail.com>
2019-01-19 16:54:03 +0100

    Initalized project

---
+++ FuncAnalyzer/Analyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FuncAnalyzer
+{
+    /// <summary>
+    /// Function analyzer
+    /// </summary>
+    public class Analyzer
+    {
+        #region Constructor
+        public Analyzer()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        public Func<double,double> Function { get; protected set; }
+        #endregion
+
+        #region Methods
+            
+        #endregion
+    }
+}
---
+++ FuncAnalyzer/Derivative.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncAnalyzer
+{
+    public class Derivative
+    {
+        #region Constructor
+        public Derivative()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Classes
+
+        #endregion
+    }
+}
