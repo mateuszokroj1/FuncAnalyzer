63fe302ed285f0ede25ece5b26a13b49df7d7ba0
Mateusz Okrój <mateuszokroj1@hotmail.com>
2019-01-20 17:25:40 +0100

    Added range class.

---
+++ FuncAnalyzer/Range.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncAnalyzer
+{
+    public class Range
+    {
+        public Range()
+        {
+
+        }
+
+        public bool IsValidValue()
+        { }
+
+        public static readonly Range Natural = new Range();
+    }
+}
